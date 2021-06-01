ab6b5ad01ef40d4ef4c6589424d06e01a600c040
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-06-01 18:07:24 +0530

    Ability To Populate Address Book List From The Local Storage

--- js/addressHome.js
+++ js/addressHome.js
@@ -1,51 +1,32 @@
+let addressList;
 window.addEventListener('DOMContentLoaded', (event) => {
+    addressList = getAddressDataFromStorage();
+    //document.querySelector("person-count").textContent = addressList.length;
     createInnerHtml();
 });
 
-const createInnerHtml = () => {
-    const headerHtml = "<th></th><th>Full Name</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th><th></th>"
-    let innerHtml2 = `${headerHtml}`;
-    let addressList = createAddressBookJSON();
-    for (const addressData of addressList) {
-        innerHtml = `${innerHtml2}
-        <tr>
-            <td></td>
-            <td>${addressData._name}</td>
-            <td>${addressData._address}</td>
-            <td>${addressData._city}</td>
-            <td>${addressData._state}</td>
-            <td>${addressData._zipCode}</td>
-            <td>${addressData._phoneNumber}</td>
-            <td>
-                <img id="1" onclick="remove(this)" alt="delete" src="../assets/delete-black-18dp.svg">
-                <img id="1" onclick="update(this)" alt="edit" src="../assets/create-black-18dp.svg">
-            </td>
-        </tr>`
-        ;
-    }
-    document.querySelector('#table-display').innerHTML = innerHtml;
+const getAddressDataFromStorage = () => {
+    return localStorage.getItem('AddressBook') ? JSON.parse(localStorage.getItem('AddressBook')) : [];
 }
 
-const createAddressBookJSON = () => {
-    let localAddress = [
-        {
-            _name = 'Vijay',
-            _address = '31-36-110 Rajeev Nagar',
-            _city = 'Visakhapatnam',
-            _state = 'Andhra Pradesh',
-            _zipCode = '530046',
-            _phoneNumber = '91 8977890300',
-            _id = new Date().getTime
-        },
-        {
-            _name = 'Kumar',
-            _address = '5-4-2 City Nagar',
-            _city = 'Pune',
-            _state = 'Maharastra',
-            _zipCode = '300291',
-            _phoneNumber = '91 8786892320',
-            _id = new Date().getTime
-        }
-    ];
-    return localAddress;
+const createInnerHtml = () => {
+    const headerHtml = "<th>Full Name</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th><th></th>"
+    if (addressList.length == 0) return;
+    let innerHtml = `${headerHtml}`;
+    for (const addressBookData of addressList) {
+        innerHtml = `${innerHtml}
+        <tr>
+            <td>${addressBookData._name}</td>
+            <td>${addressBookData._address}</td>
+            <td>${addressBookData._city}</td>
+            <td>${addressBookData._state}</td>
+            <td>${addressBookData._zipCode}</td>
+            <td>${addressBookData._phoneNumber}</td>
+            <td>
+                <img name="${addressBookData._id}" onclick="remove(this) alt="delete" src="../assets/delete-black-18dp.svg">
+                <img name="${addressBookData._id}" onclick="update(this) alt="edit" src="../assets/create-black-18dp.svg">
+            </td>
+        </tr>
+    `;}
+    document.querySelector('#table-display').innerHTML = innerHtml;
 }
